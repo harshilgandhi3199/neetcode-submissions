cc5cd300d6d1b705fc98b72fccf5c55735113e58
Harshil Gandhi <42978038+harshilgandhi3199@users.noreply.github.com>
2026-06-19 17:17:21 -0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = []
+
+        hash_map = {}
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in hash_map:
+                hash_map[sorted_word].append(word)
+            else:
+                hash_map[sorted_word] = [word]
+
+        for value in hash_map.values():
+            result.append(value)
+
+        return result
