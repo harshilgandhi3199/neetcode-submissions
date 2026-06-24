adf2c283f63513161a1902489461f21aad0d2656
Harshil Gandhi <42978038+harshilgandhi3199@users.noreply.github.com>
2026-06-23 22:58:35 -0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # "hello", "world" -> "5/hello5/world"
+        # keep length and string together
+        encoded_str = ""
+        for s in strs:
+            count = len(s)
+            encoded_str += f"{count}/"
+            encoded_str += s
+
+        print(encoded_str)
+        return encoded_str
+    
+    def decode(self, s: str) -> List[str]:
+        decoded_strs = []
+        "5/hello5/world"
+        i = 0
+        count = ""
+        while i < len(s):
+            if s[i] != '/':
+                count += s[i]
+                i += 1
+                continue
+            count = int(count)
+            curr_str = s[i+1: i+1+count] 
+            decoded_strs.append(curr_str)
+            i = i + 1 + count
+            count = ""
+            
+        return decoded_strs
